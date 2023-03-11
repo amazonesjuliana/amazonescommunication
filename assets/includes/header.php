46167546fa13ee1203618fa976f99211a4e624cf
Gustavo Scavazza <ggscavazza.java@gmail.com>
2023-03-11 14:43:11 -0300

    Add: tela SOBRE e suas dependências

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -1,13 +1,26 @@
+<?php
+    $url = $_SERVER['PHP_SELF'];
+    $pag_atual = substr($url,23);
+?>
+
 <div class="header">
     <div class="logo-header">
-        <img src="./assets/logos/amazones.svg" alt="Logo da empresa Amazones Communication">
+        <a href="./"><img src="./assets/logos/amazones.png" alt="Logo da empresa Amazones Communication"></a>
     </div>
 
     <div class="nav-header">
-
+        <a href="./portifolio.php" <?php if($pag_atual == "portifolio.php"){ ?>class="pag-atual"<?php } ?>>PORTFÓLIO</a>
+        <span>|</span>
+        <a href="./sobre.php" <?php if($pag_atual == "sobre.php"){ ?>class="pag-atual"<?php } ?>>SOBRE</a>        
+        <span>|</span>
+        <a href="./parceiros.php" <?php if($pag_atual == "parceiros.php"){ ?>class="pag-atual"<?php } ?>>PARCEIROS</a>
+        <span>|</span>
+        <a href="./contato.php" <?php if($pag_atual == "contato.php"){ ?>class="pag-atual"<?php } ?>>CONTATO</a>
     </div>
-
+        
     <div class="idiomas-header">
-
+        <img src="./assets/images/bandeira_brasil.jpg" alt="Bandeira do Brasil">
+        <img src="./assets/images/bandeira_reino_unido.jpg" alt="Bandeira do Reino Unido">
+        <img src="./assets/images/bandeira_espanha.jpg" alt="Bandeira da Espanha">
     </div>
 </div>
--- index.php
+++ index.php
@@ -1,9 +1,21 @@
-<html>
+<html style="overflow-y: hidden;">
     <head>
         <?php include("./assets/includes/head.php"); ?>
     </head>
 
     <body>
         <?php include("./assets/includes/header.php"); ?>
+
+        <div class="conteudo-home">
+            <img src="./assets/images/home.jpg" alt="Mulher tecnológica">
+            
+            <div class="textos-home">
+                <p id="texto-home1">“Paixão, comunicação e arte.”</p>
+                <p id="texto-home2">Somos apaixonados pela arte de comunicar.</p>
+                <p id="texto-home3">Oferecemos soluções através do digital e design para nossos clientes.</p>
+            </div>
+
+            <a href="#" class="btn-home">SAIBA MAIS</a>
+        </div>
     </body>
 </html>

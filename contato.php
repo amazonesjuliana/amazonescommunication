d218c5147062421177384a0484c86b81eb594e21
Gustavo Scavazza <ggscavazza.java@gmail.com>
2023-03-13 10:46:40 -0300

    Fix: ajustando responsividade

---
+++ contato.php
@@ -0,0 +1,62 @@
+<html style="overflow-y: hidden;">
+    <head>
+        <?php include("./assets/includes/head.php"); ?>
+    </head>
+
+    <body>
+        <?php include("./assets/includes/header.php"); ?>
+        <?php include("./assets/includes/fundo.php"); ?>        
+        
+        <div class="conteudo-sobre">
+            <div class="foto-juju">
+                <img src="./assets/images/juliana-profissional.jpg" alt="Foto da profissional Juliana">
+            </div>
+            
+            <div class="textos-sobre">
+                <p class="titulos-sobre">SOLUÇÕES OFERECIDAS</p>
+                <p class="texto-sobre">
+                    Prestamos serviços de marketing digital, mídias sociais, e-commerce e design gráfico, por isso, oferecemos serviços de comunicação através do digital.<br>
+
+                    Sites, aplicativos e desenvolvimento de sistemas também são alguns dos serviços que prestamos, em parceria com a <a href="https://genesisdevstudio.com" target="_blank"><b>Genesis Dev Studio</b></a>.<br>
+
+                    Através de um trabalho integrado e personalizado para cada cliente, podemos atingir os objetivos traçados em cada projeto.<br>
+                </p><br><br>
+
+                <p class="titulos-sobre">EXPERIÊNCIA PROFISSIONAL</p>
+                <p class="texto-sobre">Sou formada em publicidade com ênfase em direção de arte, com experiência em social media, e-commerce, marketing digital e eventos, possuo uma visão 360° da comunicação.</p>
+
+                <p class="icones-sobre">
+                    <span>
+                        <img src="./assets/images/icone_dev_de_sites_e_apps.png" alt="Ícone desenvolvimento de sites e aplicativos">
+                    </span>
+
+                    <span>
+                        <img src="./assets/images/icone_criacao_digital_e_impresso.png" alt="Ícone criação e design digital e impresso">
+                    </span>
+
+                    <span>
+                        <img src="./assets/images/icone_mkt_de_conteudo.png" alt="Ícone marketing de conteúdo">
+                    </span>
+
+                    <span>
+                        <img src="./assets/images/icone_mkt_digital.png" alt="Ícone marketing digital">
+                    </span>
+
+                    <span>
+                        <img src="./assets/images/icone_ecommerce.png" alt="Ícone e-commerce">
+                    </span>
+
+                    <span>
+                        <img src="./assets/images/icone_midias_sociais.png" alt="Ícone mídias sociais">
+                    </span>
+
+                    <span>
+                        <img src="./assets/images/icone_metricas_e_analises.png" alt="Ícone métricas e análises">
+                    </span>
+                </p>
+            </div>
+
+            <a href="./contato.php" class="btn-sobre">entre em contato</a>
+        </div>
+    </body>
+</html>
